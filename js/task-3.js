c9da0460b2fcd2200492c73006dcbd4b270f4416
CactusW <chernyavskij5@gmail.com>
2024-06-18 23:58:50 +0300

    fix_task_3

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,5 @@
 const sortByDescendingFriendCount = users => 
-    users.slice().sort((a, b) => b.friends.length - a.friends.length);
+    users.sort((a, b) => b.friends.length - a.friends.length);
 
 console.log(
   sortByDescendingFriendCount([
